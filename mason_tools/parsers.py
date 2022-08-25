161d5f461b8066682f92208859a9fccb861135e1
Bruno FS Ciconelle <fsouza.bruno@gmail.com>
2022-08-24 23:51:44 -0300

    Parse build dependencies from setup.cfg

--- mason_tools/parsers.py
+++ mason_tools/parsers.py
@@ -1,3 +1,5 @@
+from configparser import ConfigParser
+
 import toml
 
 
@@ -6,3 +8,14 @@ def parse_pyproject_toml(content):
         return toml.loads(content)["build-system"]["requires"]
     except KeyError:
         return []
+
+
+def parse_setup_cfg(content):
+    config = ConfigParser()
+    config.read_string(content)
+    try:
+        build_requirements = config["options"]["setup_requires"]
+    except KeyError:
+        return []
+
+    return [req.strip() for req in build_requirements.strip().splitlines()]
--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -1,6 +1,6 @@
 import pytest
 
-from mason_tools.parsers import parse_pyproject_toml
+from mason_tools.parsers import parse_pyproject_toml, parse_setup_cfg
 
 EXAMPLE_TOML = """
 [build-system]
@@ -25,3 +25,33 @@ target-version = ["py310"]
 )
 def test_parse_toml(toml, expected_result):
     assert parse_pyproject_toml(toml) == expected_result
+
+
+EXAMPLE_CFG = """
+[metadata]
+name = foo
+
+[options]
+# comments are not supported inside keywords
+setup_requires =
+    foo
+    bar
+"""
+
+EXAMPLE_CFG_WO_SETUP_REQUIRES = """
+[metadata]
+name = foo
+
+[options]
+install_requires =
+    foo
+    bar
+"""
+
+
+@pytest.mark.parametrize(
+    "setup_cfg,expected_result",
+    [(EXAMPLE_CFG, ["foo", "bar"]), (EXAMPLE_CFG_WO_SETUP_REQUIRES, [])],
+)
+def test_parse_setup_cfg(setup_cfg, expected_result):
+    assert parse_setup_cfg(setup_cfg) == expected_result
