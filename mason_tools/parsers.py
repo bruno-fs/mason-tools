8566213b68b05acf152966906cfcadb131324142
Bruno FS Ciconelle <fsouza.bruno@gmail.com>
2022-08-25 02:04:27 -0300

    Parse build dependencies from setup.py
    Because setup.py is actual code the possibilities are endless. Covering
    all possible use cases is not itentended.

--- mason_tools/parsers.py
+++ mason_tools/parsers.py
@@ -1,3 +1,4 @@
+import ast
 from configparser import ConfigParser
 
 import toml
@@ -19,3 +20,61 @@ def parse_setup_cfg(content):
         return []
 
     return [req.strip() for req in build_requirements.strip().splitlines()]
+
+
+def parse_setup_py(content):
+    code_tree = ast.parse(content)
+    setup_expr = _get_setup_expr(code_tree)
+    if setup_expr is None:
+        # module don't have a setup call
+        return []
+
+    deps_ast = _get_setup_requires(setup_expr, code_tree)
+    return _resolve_deps(deps_ast, code_tree)
+
+
+def _get_setup_expr(module: ast.Module) -> ast.Expr:
+    for elem in module.body:
+        if (
+            isinstance(elem, ast.Expr)
+            and isinstance(elem.value, ast.Call)
+            and elem.value.func.id == "setup"
+        ):
+            return elem
+
+
+def _get_setup_requires(setup_expr: ast.Expr, module: ast.Module):
+    for kw in setup_expr.value.keywords:
+        if kw.arg == "setup_requires":
+            return kw.value
+
+
+def _resolve_name(name: ast.Name, module: ast.Module):
+    for elem in module.body:
+        if isinstance(elem, ast.Assign):
+            assert len(elem.targets) == 1, "Unsupported setup.py"
+            assert isinstance(elem.targets[0], ast.Name), "Unsupported setup.py"
+            if elem.targets[0].id == name.id:
+                if isinstance(elem.value, ast.Name):
+                    return _resolve_name(elem.value, module)
+                return elem.value
+
+
+def _value_getter(element, module: ast.Module):
+    if isinstance(element, ast.Constant):
+        return element.value
+    elif isinstance(element, ast.Name):
+        target_element = _resolve_name(element, module)
+        return _value_getter(target_element, module)
+    raise NotImplementedError()
+
+
+def _resolve_deps(deps_ast, module: ast.Module):
+    if deps_ast is None:
+        return []
+    if isinstance(deps_ast, ast.List):
+        return [_value_getter(el, module) for el in deps_ast.elts]
+    elif isinstance(deps_ast, ast.Name):
+        value = _resolve_name(deps_ast, module)
+        return _resolve_deps(value, module)
+    raise NotImplementedError()
--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -1,6 +1,6 @@
 import pytest
 
-from mason_tools.parsers import parse_pyproject_toml, parse_setup_cfg
+from mason_tools.parsers import parse_pyproject_toml, parse_setup_cfg, parse_setup_py
 
 EXAMPLE_TOML = """
 [build-system]
@@ -55,3 +55,90 @@ install_requires =
 )
 def test_parse_setup_cfg(setup_cfg, expected_result):
     assert parse_setup_cfg(setup_cfg) == expected_result
+
+
+EXAMPLE_SETUP_PY = """
+from setuptools import setup
+
+setup()
+"""
+
+EXAMPLE_SETUP_PY_DIRECT = """
+from setuptools import setup
+
+setup(
+    setup_requires=[
+        "foo",  # comments should not be a problem
+    ],
+)
+"""
+
+
+EXAMPLE_SETUP_PY_CONSTANT = """
+from setuptools import setup
+
+SETUP_REQUIRES = [
+    "bar",  # some comment
+]
+
+setup(
+    setup_requires=SETUP_REQUIRES,
+)
+"""
+
+EXAMPLE_SETUP_PY_NO_SETUP = """
+def foo():
+    ...
+"""
+
+EXAMPLE_SETUP_PY_INDIRECT = """
+from setuptools import setup
+DEP = "baz"
+SETUP_REQUIRES = [DEP]
+setup(setup_requires=SETUP_REQUIRES)
+"""
+
+EXAMPLE_SETUP_PY_IMPORTED = """
+from setuptools import setup
+from foo import bar
+setup(setup_requires=bar)
+"""
+
+# next two are far fetch
+EXAMPLE_SETUP_PY_KWARGS_DICT_CALL = """
+from setuptools import setup
+
+kwargs = dict(setup_requires="foo")
+setup(**kwargs)
+"""
+EXAMPLE_SETUP_PY_KWARGS_DICT_LITERAL = """
+from setuptools import setup
+
+kwargs = {"setup_requires": "foo"}
+setup(**kwargs)
+"""
+
+
+@pytest.mark.parametrize(
+    "setup_py,expected_result",
+    [
+        (EXAMPLE_SETUP_PY, []),
+        (EXAMPLE_SETUP_PY_CONSTANT, ["bar"]),
+        (EXAMPLE_SETUP_PY_DIRECT, ["foo"]),
+        (EXAMPLE_SETUP_PY_IMPORTED, []),
+        (EXAMPLE_SETUP_PY_INDIRECT, ["baz"]),
+        (EXAMPLE_SETUP_PY_NO_SETUP, []),
+        pytest.param(
+            EXAMPLE_SETUP_PY_KWARGS_DICT_CALL,
+            ["foo"],
+            marks=pytest.mark.xfail(reason="not implemented"),
+        ),
+        pytest.param(
+            EXAMPLE_SETUP_PY_KWARGS_DICT_LITERAL,
+            ["foo"],
+            marks=pytest.mark.xfail(reason="not implemented"),
+        ),
+    ],
+)
+def test_parse_setup_py(setup_py, expected_result):
+    assert parse_setup_py(setup_py) == expected_result
